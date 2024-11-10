5b6dc52e0dd7a97d9560415a11d689b5096af820
Allan Taylor <allan.taylor.pi@gmail.com>
2024-11-10 23:14:20 +1300

    2024 Quest 4
    Now blazing fast! Uses the median (well, sorts the numbers to find it - rather than importing median from stats) to determine viable targets. For an even number of nails, the target can be any value between the "medians" - these all produce the same result

--- 2024/04.py
+++ 2024/04.py
@@ -1,4 +1,9 @@
 for part in [1,2,3]:
     with open(f"everybody_codes_e2024_q4_p{part}.txt") as f:
         nums = list(map(int,f.read().splitlines()))
-    print(min(sum(abs(num-target) for num in nums) for target in (range(min(nums),max(nums)+1) if part == 3 else [min(nums)])))
+        if part == 3:
+            nums.sort()
+            target = nums[len(nums)//2]
+        else:
+            target = min(nums)
+    print(sum(abs(num-target) for num in nums))
