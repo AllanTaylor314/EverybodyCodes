c9006e2005090fa54d74208f329771fffa71ad74
Allan Taylor <allan.taylor.pi@gmail.com>
2024-11-10 23:40:40 +1300

    2024 Quest 5 Part 2
    Ran with PyPy

--- 2024/05.py
+++ 2024/05.py
@@ -1,11 +1,15 @@
+from collections import Counter
 from itertools import zip_longest
 
-with open("everybody_codes_e2024_q5_p1.txt") as f:
+with open("everybody_codes_e2024_q5_p2.txt") as f:
     lines = f.read().splitlines()
 nums = [list(map(int,line.split())) for line in lines]
 cols = list(map(list,zip(*nums)))
 
-for r in range(10):
+seen_numbers = Counter()
+result = None
+r = 0
+while seen_numbers[result] < 2024:
     clapper_column = r%len(cols)
     clapper = cols[clapper_column].pop(0)
     target_col = cols[(clapper_column+1)%len(cols)]
@@ -13,7 +17,9 @@ for r in range(10):
     if position >= len(target_col):
         position = 2*len(target_col) - position
     target_col.insert(position,clapper)
-    print(f"Round {r+1}")
-    for row in zip_longest(*cols,fillvalue=" "):
-        print(*row)
-print(*(col[0] for col in cols))
+    result = int("".join(str(col[0]) for col in cols))
+    seen_numbers[result] += 1
+    r += 1
+print(result*r)
+# Not 10111010 (wrong length, right first digit)
+# Missed the multiply by num rounds
