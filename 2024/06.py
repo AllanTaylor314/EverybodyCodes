74b254312cf986846ee77666ff582569a86e5d9f
Allan Taylor <allan.taylor.pi@gmail.com>
2024-11-13 13:27:08 +1300

    2024 Quest 6

--- 2024/06.py
+++ 2024/06.py
@@ -1,13 +1,5 @@
 from collections import defaultdict, Counter
 
-with open("everybody_codes_e2024_q06_p1.txt") as f:
-    lines = f.read().splitlines()
-
-adj_mat = defaultdict(list)
-for line in lines:
-    par, *chdrn = line.replace(":",",").split(",")
-    adj_mat[par] = chdrn
-
 def gen_paths(matrix, root='RR', truncate=None, visited=None):
     if visited is None:
         visited = set()
@@ -19,32 +11,15 @@ def gen_paths(matrix, root='RR', truncate=None, visited=None):
     for subroot in matrix[root]:
         for path in gen_paths(matrix, subroot, truncate, visited|{root}):
             yield root[:truncate]+path
-
-paths = list(gen_paths(adj_mat))
-lengths = Counter(map(len,paths))
-powerfull_length, = (k for k,v in lengths.items() if v==1)
-print(*(path for path in paths if len(path) == powerfull_length))
-# Not MKXCQTQFSR@ (argh - it'll be RRMKXCQTQFSR@)
-
-with open("everybody_codes_e2024_q06_p2.txt") as f:
-    lines = f.read().splitlines()
+for part in [1,2,3]:
+    with open(f"everybody_codes_e2024_q06_p{part}.txt") as f:
+        lines = f.read().splitlines()
     adj_mat = defaultdict(list)
-for line in lines:
-    par, *chdrn = line.replace(":",",").split(",")
-    adj_mat[par] = chdrn
-paths = list(gen_paths(adj_mat,truncate=1))
-lengths = Counter(map(len,paths))
-powerfull_length, = (k for k,v in lengths.items() if v==1)
-print(*(path for path in paths if len(path) == powerfull_length))
-# Not RMXQQS@ (wrong length. Used part 1 text :fp:)
+    for line in lines:
+        par, *chdrn = line.replace(":",",").split(",")
+        adj_mat[par] = chdrn
+    paths = list(gen_paths(adj_mat,truncate=(None if part == 1 else 1)))
+    lengths = Counter(map(len,paths))
+    powerful_length, = (k for k,v in lengths.items() if v==1)
+    print(*(path for path in paths if len(path) == powerful_length))
 
-with open("everybody_codes_e2024_q06_p3.txt") as f:
-    lines = f.read().splitlines()
-    adj_mat = defaultdict(list)
-for line in lines:
-    par, *chdrn = line.replace(":",",").split(",")
-    adj_mat[par] = chdrn
-paths = list(gen_paths(adj_mat,truncate=1))
-lengths = Counter(map(len,paths))
-powerfull_length, = (k for k,v in lengths.items() if v==1)
-print(*(path for path in paths if len(path) == powerfull_length))
