e4bb6329e4a3d4101ba8ef444d62508be901a4dc
Allan Taylor <allan.taylor.pi@gmail.com>
2024-11-20 14:13:15 +1300

    2024 Quest 12 Part 3
    No incorrect submissions today :). Part 3 is still kinda slow (about a minute with PyPy, two without - but that's still faster than a refactor for this single use code), though most of that time is in building up reachable_area (who would've thought that finding 32 million locations was a bit slow!). Once that's done, the check is pretty fast. I had an issue early on where not all the meteors could be hit by launching at t=0 (unlike the example). The coordinate system differs for part 3 (since it actually matters in an absolute sense - same directions, different origin)

--- 2024/12.py
+++ 2024/12.py
@@ -1,24 +1,15 @@
+from itertools import count
+from collections import defaultdict
+
 SEGMENT_NUMBER = {"A":1, "B":2, "C":3}
 
 def load_file(part):
     with open(f"everybody_codes_e2024_q12_p{part}.txt") as f:
         return f.read().splitlines()
 
-targets = set()
-hard_targets = set()
-catapults = {}
-for i,line in enumerate(reversed(load_file(2))):
-    for j,c in enumerate(line):
-        if c == "T":
-            targets.add((i,j))
-        elif c == "H":
-            hard_targets.add((i,j))
-        elif c.isalpha():
-            catapults[c] = (i,j)
-
 def flight_path(catapult, power):
     i,j = catapults[catapult]
-    path = []
+    path = [(i,j)]
     for _ in range(power):
         i += 1
         j += 1
@@ -55,16 +46,62 @@ def all_targets(path):
     return [p for p in path if p in targets or p in hard_targets]
 
 def score(catapult, power, is_hard=False):
-    return (ord(catapult)-64) * power * (2 if is_hard else 1)
+    return SEGMENT_NUMBER[catapult] * power * (2 if is_hard else 1)
 assert score("B", 5) == 10
 
-viable_shots = {}
-for n in range(200):
-    for catapult in catapults:
-        path = flight_path(catapult, n)
-        if ts := all_targets(path):
-            viable_shots[catapult,n] = ts
+for part in [1,2]:
+    targets = set()
+    hard_targets = set()
+    catapults = {}
+    for i,line in enumerate(reversed(load_file(part))):
+        for j,c in enumerate(line):
+            if c == "T":
+                targets.add((i,j))
+            elif c == "H":
+                hard_targets.add((i,j))
+            elif c.isalpha():
+                catapults[c] = (i,j)
 
-hittable_locations = [p for v in viable_shots.values() for p in v]
-assert len(hittable_locations) == len(set(hittable_locations))
-print(sum(score(c,p,t in hard_targets) for (c,p),v in viable_shots.items() for t in v))
+    viable_shots = {}
+    for n in range(200):
+        for catapult in catapults:
+            path = flight_path(catapult, n)
+            if ts := all_targets(path):
+                viable_shots[catapult,n] = ts
+
+    hittable_locations = [p for v in viable_shots.values() for p in v]
+    assert len(hittable_locations) == len(set(hittable_locations))
+    print(sum(score(c,p,t in hard_targets) for (c,p),v in viable_shots.items() for t in v))
+
+# PART 3
+catapults = {"A":(0,0),"B":(1,0),"C":(2,0)}
+meteor_locations = [tuple(map(int,line.split()))[::-1] for line in load_file(3)]
+
+def gen_meteor_path(start):
+    i,j = start
+    while i >= 0:
+        yield i,j
+        i -= 1
+        j -= 1
+
+reachable_area = defaultdict(list)
+for catapult in "ABC":
+    for power in range(1,4010):
+        for time, location in enumerate(flight_path(catapult,power)):
+            if not any(c==catapult for c,p,t in reachable_area[location]):
+                reachable_area[location].append((catapult,power,time))
+
+print(f"Found {len(reachable_area)} reachable locations")
+
+total_cost = 0
+for meteor in meteor_locations:
+    for time,m_loc in enumerate(gen_meteor_path(meteor)):
+        options = [(c,p) for c,p,t in reachable_area[m_loc] if t <= time]
+        if options:
+            cost = min(score(c,p) for c,p in options)
+            print(meteor, time, cost)
+            total_cost += cost
+            break
+    else:
+        raise ValueError(f"Nothing found for {meteor}")
+print(total_cost)
