f9af3c10878b1d11083dc189bdf0c49041fba8fd
Allan Taylor <allan.taylor.pi@gmail.com>
2024-11-21 13:59:46 +1300

    2024 Quest 13
    Slight reorder and refactor to solve all three parts. All three now build paths from the end (since it doesn't matter for parts 1 and 2, and it really matters for part 3). On my machine, it takes about 6 seconds with PyPy or 18 with CPython, and it doesn't use 97% of my RAM like the initial queue-based attempts did

--- 2024/13.py
+++ 2024/13.py
@@ -1,15 +1,6 @@
-from collections import deque
-from heapq import heappop, heappush
 def load_file(part):
     with open(f"everybody_codes_e2024_q13_p{part}.txt") as f:
         return f.read().splitlines()
-
-lines = load_file(3)
-grid = {(i,j):c for i,line in enumerate(lines) for j,c in enumerate(line) if c not in "# "}
-
-starts = [k for k,v in grid.items() if v=="S"]
-end ,= (k for k,v in grid.items() if v=="E")
-grid = {k:int(v) if v.isnumeric() else 0 for k,v in grid.items()}
 
 def adjacent_spaces(point):
     i, j = point
@@ -21,15 +12,23 @@ def valid_adjacent_spaces(point):
 def min_transition(a,b):
     return min(abs(a-b),abs(10-a+b),abs(10-b+a))
 
-costs = {p:1e80 for p in grid}
-costs[end] = 0
-updated = True
-while updated:
-    updated = False
-    for point in costs:
-        adjs = valid_adjacent_spaces(point)
-        new_cost = min([costs[adj] + min_transition(grid[point],grid[adj])+1 for adj in adjs] or [1e8])
-        if new_cost < costs[point]:
-            costs[point] = new_cost
-            updated = True
-print(min(costs[start] for start in starts))
+for part in (1,2,3):
+    lines = load_file(part)
+    grid = {(i,j):c for i,line in enumerate(lines) for j,c in enumerate(line) if c not in "# "}
+
+    starts = [k for k,v in grid.items() if v=="S"]
+    end ,= (k for k,v in grid.items() if v=="E")
+    grid = {k:int(v) if v.isnumeric() else 0 for k,v in grid.items()}
+
+    costs = {p:1e80 for p in grid}
+    costs[end] = 0
+    updated = True
+    while updated:
+        updated = False
+        for point in costs:
+            adjs = valid_adjacent_spaces(point)
+            new_cost = min([costs[adj] + min_transition(grid[point],grid[adj])+1 for adj in adjs] or [1e8])
+            if new_cost < costs[point]:
+                costs[point] = new_cost
+                updated = True
+    print(min(costs[start] for start in starts))
