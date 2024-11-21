44fcefe1bf698be35c6cf90876472fbff67f9a49
Allan Taylor <allan.taylor.pi@gmail.com>
2024-11-21 13:38:03 +1300

    2024 Quest 13 Parts 1 & 2

---
+++ 2024/13.py
@@ -0,0 +1,39 @@
+from collections import deque
+from heapq import heappop, heappush
+def load_file(part):
+    with open(f"everybody_codes_e2024_q13_p{part}.txt") as f:
+        return f.read().splitlines()
+
+lines = load_file(2)
+grid = {(i,j):c for i,line in enumerate(lines) for j,c in enumerate(line) if c not in "# "}
+
+start ,= (k for k,v in grid.items() if v=="S")
+end ,= (k for k,v in grid.items() if v=="E")
+grid[start] = "0"
+grid[end] = "0"
+grid = {k:int(v) for k,v in grid.items()}
+
+def adjacent_spaces(point):
+    i, j = point
+    return [(i-1,j),(i,j-1),(i,j+1),(i+1,j)]
+
+def valid_adjacent_spaces(point):
+    return [p for p in adjacent_spaces(point) if p in grid]
+
+def min_transition(a,b):
+    return min(abs(a-b),abs(10-a+b),abs(10-b+a))
+
+costs = {p:1e80 for p in grid}
+costs[start] = 0
+updated = True
+while updated:
+    updated = False
+    for point in costs:
+        # point = q.pop()
+        adjs = valid_adjacent_spaces(point)
+        # q.extend(adjs)
+        new_cost = min((costs[adj] + min_transition(grid[point],grid[adj])+1 for adj in adjs))
+        if new_cost < costs[point]:
+            costs[point] = new_cost
+            updated = True
+print(costs[end])
