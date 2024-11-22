396d15a3fa563ed582245e4348707a9f031b96d1
Allan Taylor <allan.taylor.pi@gmail.com>
2024-11-23 01:57:04 +1300

    2024 Quest 5
    Reincorporate parts 1 & 2, and make part 3 terminate (the funky \r carriage return trick isn't strictly necessary since it takes less than 4 seconds to terminate properly, but where's the fun in that)

--- 2024/05.py
+++ 2024/05.py
@@ -1,12 +1,13 @@
-with open("everybody_codes_e2024_q5_p3.txt") as f:
-    lines = f.read().splitlines()
-nums = [list(map(int,line.split())) for line in lines]
-cols = list(map(list,zip(*nums)))
+from collections import Counter
 
-result = None
-r = 0
-highest = 0
-while True:
+def load_file(part):
+    with open(f"everybody_codes_e2024_q5_p{part}.txt") as f:
+        lines = f.read().splitlines()
+    nums = [list(map(int,line.split())) for line in lines]
+    cols = list(map(list,zip(*nums)))
+    return cols
+
+def step(r, cols):
     clapper_column = r%len(cols)
     clapper = cols[clapper_column].pop(0)
     target_col = cols[(clapper_column+1)%len(cols)]
@@ -14,8 +15,38 @@ while True:
     if position >= len(target_col):
         position = 2*len(target_col) - position
     target_col.insert(position,clapper)
+
+def get_state(r, cols):
+    return (r%len(cols),tuple(map(tuple,cols)))
+
+cols = load_file(1)
+for r in range(10):
+    step(r, cols)
+print(*(col[0] for col in cols),sep="")
+
+cols = load_file(2)
+seen_numbers = Counter()
+result = None
+r = 0
+while seen_numbers[result] < 2024:
+    step(r,cols)
+    result = int("".join(str(col[0]) for col in cols))
+    seen_numbers[result] += 1
+    r += 1
+print(result*r)
+
+cols = load_file(3)
+r = 0
+highest = 0
+states = set()
+while True:
+    step(r, cols)
     result = int("".join(str(col[0]) for col in cols))
     if result > highest:
-        highest = result
-        print(highest)
+        print(end=f"\r{(highest:=result)}")
     r += 1
+    state = get_state(r, cols)
+    if state in states:
+        break
+    states.add(state)
+print()
