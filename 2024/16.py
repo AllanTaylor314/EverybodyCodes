7d478b3341b4ebf6789e28f2d16a7c1ce237be6e
Allan Taylor <allan.taylor.pi@gmail.com>
2024-11-26 14:06:45 +1300

    2024 Quest 16 Part 2

--- 2024/16.py
+++ 2024/16.py
@@ -1,19 +1,46 @@
-from itertools import zip_longest
+from itertools import zip_longest, count
+from operator import mul
+from functools import reduce, cache
+from collections import Counter
+from math import lcm
+
+GOAL = 202420242024
 
 def load_file(part):
     with open(f"everybody_codes_e2024_q16_p{part}.txt") as f:
         header, _, *remainder = f.read().splitlines()
     wheel_distances = list(map(int,header.split(",")))
     wheels_transpose = [[line[i:i+3].strip() for i in range(0,len(line),4)] for line in remainder]
-    wheels = [list(filter(None,wheel)) for wheel in zip_longest(*wheels_transpose,fillvalue="")]
+    wheels = [tuple(filter(None,wheel)) for wheel in zip_longest(*wheels_transpose,fillvalue="")]
     return wheel_distances, wheels
 
+def get_line(wheel_positions, wheels):
+    return " ".join(wheel[i] for i,wheel in zip(wheel_positions,wheels))
+
+def get_ith_line(i, wheels):
+    return " ".join(wheel[i%len(wheel)] for wheel in wheels)
+
+def score_line(line):
+    line = line.replace(" ","")
+    return sum(i-2 for i in Counter(line).values() if i>2)
+
 wheel_distances, wheels = load_file(1)
-wheel_positions = [0] * len(wheel_distances)
-# wheel_positions = list(map(sum,zip(wheel_positions,wheel_distances)))
-wheel_positions = [(a+b)*100%len(c) for a,b,c in zip(wheel_positions,wheel_distances,wheels)]
+wheels = [tuple(wheel[i%len(wheel)] for i,_ in zip(count(step=dist),wheel)) for dist,wheel in zip(wheel_distances,wheels)]
+del wheel_distances
 
-print(*(wheel[i] for i,wheel in zip(wheel_positions,wheels)))
+print(get_ith_line(100, wheels))
 
-# Not <,- <:> -,* <,^ (correct first char and len)
-# It was <,- <.< *,- <,^ (which is the result of 100 spins, not one more spin)
+wheel_distances, wheels = load_file(2)
+wheels = [tuple(wheel[i%len(wheel)][::2] for i,_ in zip(count(step=dist),wheel)) for dist,wheel in zip(wheel_distances,wheels)]
+del wheel_distances
+
+loop_size = lcm(*map(len,wheels))
+loop_count, loop_remainder = divmod(GOAL, loop_size)
+
+score_loop = 0
+score_rem = 0
+for i in range(1,loop_size+1):
+    score_loop += score_line(get_ith_line(i,wheels))
+    if i == loop_remainder:
+        score_rem = score_loop
+print(score_loop * loop_count + score_rem)
