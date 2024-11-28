e28e3f9c4fed028aa5918fac70f9f048995d33d1
Allan Taylor <allan.taylor.pi@gmail.com>
2024-11-29 12:14:52 +1300

    2024 Quest 19 Part 1

---
+++ 2024/19.py
@@ -0,0 +1,34 @@
+from itertools import cycle
+
+def load_file(part):
+    with open(f"everybody_codes_e2024_q19_p{part}.txt") as f:
+        lines = f.read().splitlines()
+    return lines
+
+def adjacent_spaces(point):
+    i, j = point
+    return [(i-1,j-1),(i-1,j),(i-1,j+1),(i,j+1),(i+1,j+1),(i+1,j),(i+1,j-1),(i,j-1)]
+
+def flatten(grid):
+    return "".join(map("".join,grid))
+
+def rotate(grid, point, direction):
+    adj = adjacent_spaces(point)
+    vals = [grid[i][j] for (i,j) in adj]
+    if direction == "L":
+        adj = adj[-1:]+adj[:-1]
+    elif direction == "R":
+        adj = adj[1:]+adj[:1]
+    else:
+        raise ValueError
+    for (i,j),c in zip(adj,vals):
+        grid[i][j] = c
+
+pattern,_,*lines = load_file(1)
+grid = [list(line) for line in lines]
+rotation_points = [(i,j) for i in range(1,len(grid)-1) for j in range(1,len(grid[i])-1)]
+
+for d, p in zip(cycle(pattern), rotation_points):
+    rotate(grid, p, d)
+
+print(flatten(grid).lstrip("+>").rstrip("<+"))
