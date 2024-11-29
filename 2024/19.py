c497e8bd8979a557f054b05402d12878a08944dc
Allan Taylor <allan.taylor.pi@gmail.com>
2024-11-29 13:50:27 +1300

    2024 Quest 19
    Chucked it in a loop so it solves all 3 parts and got rid of the assertion (now that I know it works). This version doesn't show the full picture, but you can remove one line to change that. The whole thing is pretty fast, so I'm happy with that

--- 2024/19.py
+++ 2024/19.py
@@ -1,17 +1,11 @@
-"""
-For any location, the final position can probably be found individually
-(and probably also back-propagated). If I find the end location of > and <
-and the start location of everything between, I can find the value
-"""
-
 from itertools import cycle
-from pprint import pprint
 
+TARGETS = {1:1, 2:100, 3:1048576000}
 
 def load_file(part):
     with open(f"everybody_codes_e2024_q19_p{part}.txt") as f:
-        lines = f.read().splitlines()
-    return lines
+        pattern,_,*lines = f.read().splitlines()
+    return pattern,lines
 
 def adjacent_spaces(point):
     i, j = point
@@ -35,40 +29,34 @@ def rotate(grid, point, direction):
 def is_adj(point1, point2):
     return max(abs(a-b) for a,b in zip(point1,point2))==1
 
-def rotate_point(point,cor,direction):
-    adjs = adjacent_spaces(cor)
-    return adjs[adjs.index(point) + (1 if direction=="R" else -1)]
+for part in (1,2,3):
+    target = TARGETS[part]
+    pattern,lines = load_file(part)
+    grid = [list(line) for line in lines]
+    rotation_points = [(i,j) for i in range(1,len(grid)-1) for j in range(1,len(grid[i])-1)]
 
-part = 3
-TARGET = {1:1,2:100,3:1048576000}[part]
-pattern,_,*lines = load_file(part)
-grid = [list(line) for line in lines]
-rotation_points = [(i,j) for i in range(1,len(grid)-1) for j in range(1,len(grid[i])-1)]
+    loc_grid = [[(i,j) for j,c in enumerate(line)] for i,line in enumerate(lines)]
+    for d, p in zip(cycle(pattern), rotation_points):
+        rotate(loc_grid, p, d)
+    mappings = {}
+    mappings[1] = {(i,j):(oi,oj) for i,l in enumerate(loc_grid) for j,(oi,oj) in enumerate(l)}
+    mapping_size = 1
+    while mapping_size < target:
+        pmp = mappings[mapping_size]
+        mapping_size *= 2
+        mappings[mapping_size] = {k:pmp[v] for k,v in pmp.items()}
 
-loc_grid = [[(i,j) for j,c in enumerate(line)] for i,line in enumerate(lines)]
-for d, p in zip(cycle(pattern), rotation_points):
-    rotate(loc_grid, p, d)
-mappings = {}
-mappings[1] = {(i,j):(oi,oj) for i,l in enumerate(loc_grid) for j,(oi,oj) in enumerate(l)}
-mapping_size = 1
-while mapping_size < max(TARGET,2):
-    pmp = mappings[mapping_size]
-    mapping_size *= 2
-    mappings[mapping_size] = {k:pmp[v] for k,v in pmp.items()}
+    required_mappings = [k for k in mappings if k&target]
 
-for d, p in zip(cycle(pattern), rotation_points):
-    rotate(loc_grid, p, d)
-assert mappings[2] == {(i,j):(oi,oj) for i,l in enumerate(loc_grid) for j,(oi,oj) in enumerate(l)}
+    new_grid = [line.copy() for line in grid]
 
-required_mappings = [k for k in mappings if k&TARGET]
-
-new_grid = [line.copy() for line in grid]
-
-for i,line in enumerate(grid):
-    for j,c in enumerate(line):
-        oi,oj = i,j
-        for rm in required_mappings:
-            oi,oj = mappings[rm][oi,oj]
-        new_grid[i][j] = grid[oi][oj]
-
-print(flatten(new_grid))
+    for i,line in enumerate(grid):
+        for j,c in enumerate(line):
+            oi,oj = i,j
+            for rm in required_mappings:
+                oi,oj = mappings[rm][oi,oj]
+            new_grid[i][j] = grid[oi][oj]
+    solution = flatten(new_grid)
+    # Remove next line for full picture
+    solution = solution[solution.index(">")+1:solution.index("<")]
+    print(solution)
