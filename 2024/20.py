2cda593e72fdba74db9384a5df1d9dc4a10e9b4f
Allan Taylor <allan.taylor.pi@gmail.com>
2024-11-30 15:14:16 +1300

    2024 Quest 20 Part 2
    Doesn't work for the first test (gets 77), but works for second and actual

--- 2024/20.py
+++ 2024/20.py
@@ -1,6 +1,10 @@
 from functools import cache
 
-DELTAS = {"+":+1,"-":-2,".":-1,"S":0}
+DELTAS = {"+":+1,"-":-2,".":-1}
+CHECKPOINTS = [None, "A", "B", "C", "S"]
+CHECKPOINTS_MAP = {k:v for k,v in zip(CHECKPOINTS,CHECKPOINTS[1:])}
+CHECKPOINTS_IMAP = {k:v for v,k in CHECKPOINTS_MAP.items()} | {None:None}
+CHECKPOINTS_MAP["S"] = "S"
 
 def load_file(part):
     with open(f"everybody_codes_e2024_q20_p{part}.txt") as f:
@@ -23,12 +27,52 @@ def fly(location,prev_location,remaining_time):
         adjs.remove(prev_location)
     return DELTAS[c] + max(fly(next_loc,location,remaining_time-1) for next_loc in adjs)
 
-altitude = 1000
+altitude = 10000
 time = 100
-grid = load_file(1)
+grid = load_file(2)
+grid_locs = {v:k for k,v in grid.items() if v.isalpha()}
 
-start ,= ((ij) for ij,c in grid.items() if c == "S")
-si,sj = start
-print(altitude + fly(start,None,100))
+start = grid_locs["S"]
+cpa = grid_locs["A"]
+cpb = grid_locs["B"]
+cpc = grid_locs["C"]
 
-# not 1063 (CC)
+def next_states(state):
+    prev,curr,check = state
+    adjs = [p for p in adjacent_spaces(curr) if p != prev and grid.get(p,"#") != "#"]
+    next_check = CHECKPOINTS_MAP[check]
+    if curr == grid_locs[next_check]:
+        check = next_check
+    return [(curr,new,check) for new in adjs]
+
+def prev_states(state):
+    prev,curr,check = state
+    if prev is None:
+        return []
+    adjs = [p for p in adjacent_spaces(prev) if p != curr and grid.get(p,"#") != "#"]
+    adjs.append(None)
+    prev_check = CHECKPOINTS_IMAP[check]
+    if check is not None and prev == grid_locs[check]:
+        check = prev_check
+    return [(new,prev,check) for new in adjs]
+
+locations_of_interest = (start,cpa,cpb,cpc,start)
+start_state = (None,start,None)
+target_prev = next_states(start_state)[0][1]
+target_state = (target_prev, start, "C")
+to_update = set()
+costs = {}
+costs[start_state] = 0
+to_update.update(next_states(start_state))
+while to_update:
+    new_update = set()
+    for state in to_update:
+        prev,curr,check = state
+        adjs = prev_states(state)
+        new_cost = min([costs.get(adj,1e8) + 1 - DELTAS.get(grid[adj[1]],-1) for adj in adjs] or [1e8])
+        if new_cost < costs.get(state,1e8):
+            costs[state] = new_cost
+            new_update.update(next_states(state))
+    to_update = new_update
+
+print(costs[target_state])
