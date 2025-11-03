81323929411965a2381fe930f2b8e8b89d509fb2
Allan Taylor <allan.taylor.pi@gmail.com>
2025-11-04 12:13:22 +1300

    2025 Quest 1 cleanup

--- 2025/01.py
+++ 2025/01.py
@@ -1,37 +1,34 @@
 def to_delta(s):
     d = s[0]
     n = int(s[1:])
-    return -n if d == 'L' else n
+    return -n if d == "L" else n
+
 
 def load_file(part):
     with open(f"everybody_codes_e2025_q01_p{part}.txt") as f:
         lines = f.read().splitlines()
-    return lines
-name_str,_,dir_str = load_file(1)
-names = name_str.split(',')
-dirs = dir_str.split(',')
+    name_str, _, dir_str = lines
+    names = name_str.split(",")
+    dirs = dir_str.split(",")
+    return names, list(map(to_delta,dirs))
+
+
+names, dirs = load_file(1)
 i = 0
-for d in dirs:
-    delta = to_delta(d)
+for delta in dirs:
     i += delta
-    if i < 0: i = 0
-    if i >= len(names): i = len(names) - 1
+    if i < 0:
+        i = 0
+    if i >= len(names):
+        i = len(names) - 1
 print(names[i])
 
-name_str,_,dir_str = load_file(2)
-names = name_str.split(',')
-dirs = dir_str.split(',')
-i = 0
-for d in dirs:
-    delta = to_delta(d)
-    i += delta
-    i %= len(names)
+names, dirs = load_file(2)
+i = sum(dirs) % len(names)
 print(names[i])
 
-name_str,_,dir_str = load_file(3)
-names = name_str.split(',')
-dirs = dir_str.split(',')
-for d in dirs:
-    delta = to_delta(d) % len(names)
-    names[0],names[delta] = names[delta],names[0]
+names, dirs = load_file(3)
+for delta in dirs:
+    delta %= len(names)
+    names[0], names[delta] = names[delta], names[0]
 print(names[0])
